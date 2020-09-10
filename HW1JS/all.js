c385c462a3dbc20b5f1ce8b76425ca6f2316900e
Илья Онищенко <ilaonisenko@MacBook-Pro-Ila.local>
2020-09-10 18:48:28 +0300

    My first hw

---
+++ HW1JS/all.js
@@ -0,0 +1,5 @@
+let CountOfBread = prompt('How much bread do u need?');//We're finding out how much bread we need
+let PriceOfBread = prompt('How much does it cost?');//We're finding out how much does it cost
+let FinalPrice = CountOfBread * PriceOfBread;//The programm multiplies our variables
+alert('You need to pay ' + FinalPrice + ' UAH')//The program says us how much we need to pay in UAH
+// Если менять значения через девтулзы можно серьезно изменить ход программы, но не этой - это же простой калькулятор, причем с одним действием
